f695628ff341f230106ea9198378cd5f6901c9bc
Shu-50 <145758568+Shu-50@users.noreply.github.com>
2024-07-24 14:35:34 +0530

    Update Pokedex.jsx

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -30,22 +30,7 @@ const Pokedex = ({ selectedGeneration, setSelectedPokemon, searchTerm }) => {
       // console.log(pokemonList)
   }, [selectedGeneration]);
 
-      
-      
-    //   const fetchPokemon = async () => {
-    //     try {
-    //       const response = await fetch(generationEndpoints[selectedGeneration]);
-    //       if (!response.ok) {
-    //         throw new Error('Network response was not ok');
-    //       }
-    //       const data = await response.json();
-    //       setPokemonList(data.results);
-    //     } catch (error) {
-    //       console.error('Failed to fetch:', error);
-    //     }
-    //   }
-    //   fetchPokemon();
-    // }, [selectedGeneration]);
+
       
     const filteredPokemonList = pokemonList.filter(pokemon =>
       pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
@@ -53,7 +38,7 @@ const Pokedex = ({ selectedGeneration, setSelectedPokemon, searchTerm }) => {
 
   return (
     <div className="pokedex">
-      {/* {pokemonList.map((pokemon, index) => */}
+  
        {filteredPokemonList.map((pokemon, index) => (
           <Pokemon key={index}
                  name={pokemon.name}
@@ -66,80 +51,3 @@ const Pokedex = ({ selectedGeneration, setSelectedPokemon, searchTerm }) => {
 };
 
 export default Pokedex;
-
-
-// import React, { useState, useEffect } from 'react';
-// import Pokemon from './Pokemon';
-// import './Pokedex.css';
-
-// const allPokemonEndpoint = 'https://pokeapi.co/api/v2/pokemon?limit=10277';
-
-// const Pokedex = ({ selectedGeneration, setSelectedPokemon, searchTerm }) => {
-//   const [pokemonList, setPokemonList] = useState([]);
-//   const [loading, setLoading] = useState(true);
-
-//   useEffect(() => {
-//     const fetchAllPokemon = async () => {
-//       try {
-//         const response = await fetch(allPokemonEndpoint);
-//         if (!response.ok) {
-//           throw new Error('Network response was not ok');
-//         }
-//         const data = await response.json();
-
-//         // Fetch details for each Pokémon species to get generation info
-//         const pokemonDetailsPromises = data.results.map(pokemon =>
-//           fetch(pokemon.url).then(res => res.json())
-//         );
-//         const pokemonDetails = await Promise.all(pokemonDetailsPromises);
-
-//         const pokemonSpeciesPromises = pokemonDetails.map(pokemon =>
-//           fetch(pokemon.species.url).then(res => res.json())
-//         );
-//         const pokemonSpeciesDetails = await Promise.all(pokemonSpeciesPromises);
-
-//         // Add generation info to each Pokémon
-//         const detailedPokemonList = pokemonDetails.map((pokemon, index) => ({
-//           ...pokemon,
-//           generation: pokemonSpeciesDetails[index].generation.name,
-//         }));
-
-//         setPokemonList(detailedPokemonList);
-//         setLoading(false);
-//       } catch (error) {
-//         console.error('Failed to fetch:', error);
-//       }
-//     };
-
-//     fetchAllPokemon();
-//   }, []);
-
-//   // Filter by generation if selected
-//   const filteredPokemonList = pokemonList.filter(pokemon => {
-//     if (selectedGeneration === 'all') {
-//       return true;
-//     }
-//     return pokemon.generation === `generation-${selectedGeneration}`;
-//   }).filter(pokemon =>
-//     pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
-//   );
-
-//   return (
-//     <div className="pokedex">
-//       {loading ? (
-//         <p>Loading...</p>
-//       ) : (
-//         filteredPokemonList.map((pokemon, index) => (
-//           <Pokemon
-//             key={index}
-//             name={pokemon.name}
-//             url={pokemon.url}
-//             onClick={() => setSelectedPokemon(pokemon)}
-//           />
-//         ))
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Pokedex;
